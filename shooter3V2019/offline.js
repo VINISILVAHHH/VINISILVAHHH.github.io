b0c5578925aeb564ca495353191c292380e94bad
VINISILVAHHH <50116979+VINISILVAHHH@users.noreply.github.com>
2019-04-29 15:16:01 -0300

    Add files via upload

--- shooter3V2019/offline.js
+++ shooter3V2019/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1556561192,
+	"version": 1556561531,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -10,10 +10,13 @@
 		"images/shootbutton-sheet1.png",
 		"images/damage-sheet0.png",
 		"images/arrowup-sheet0.png",
+		"images/arrow2-sheet0.png",
 		"images/arrowleft-sheet0.png",
 		"images/arrowleft-sheet1.png",
 		"images/arrowdown-sheet0.png",
+		"images/arrowdown-sheet1.png",
 		"images/arrowright-sheet0.png",
+		"images/arrowright-sheet1.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
